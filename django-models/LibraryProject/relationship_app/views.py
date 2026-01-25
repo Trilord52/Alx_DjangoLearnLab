168832bb6481f8b7f8d03031c5c5785274973246
Trilord52 <tinbiteyonas52@gmail.com>
2026-01-25 22:11:37 +0300

    task 1 fix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
 
