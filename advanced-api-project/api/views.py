2680952f6ad6b33fd00c6dd5e10e74b9c2a52b41
Trilord52 <tinbiteyonas52@gmail.com>
2026-02-22 21:54:27 +0300

    week 12 task 1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookListView(generics.ListAPIView):
+    """
+    GET /books/
+    Returns all books.
+    Publicly accessible.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class BookDetailView(generics.RetrieveAPIView):
+    """
+    GET /books/<pk>/
+    Returns single book by ID.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class BookCreateView(generics.CreateAPIView):
+    """
+    POST /books/create/
+    Create new book.
+    Only authenticated users allowed.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    """
+    PUT /books/<pk>/update/
+    Update existing book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    """
+    DELETE /books/<pk>/delete/
+    Delete book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
