71e2a96a382f44f11cda5866d5b8127c41a532a3
Trilord52 <tinbiteyonas52@gmail.com>
2026-02-22 22:55:10 +0300

    fix 2

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -9,3 +9,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+from django import forms
+from django.contrib.auth.models import User
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -20,3 +20,22 @@ def register(request):
         form = UserRegisterForm()
 
     return render(request, 'blog/register.html', {'form': form})
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .forms import UserUpdateForm  # We'll create this form
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        form = UserUpdateForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})
