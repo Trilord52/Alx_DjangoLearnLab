b65c9055e298f36d6f8203f6d12a1dc647140d9c
Trilord52 <tinbiteyonas52@gmail.com>
2026-02-22 22:09:15 +0300

    week 12 task 2 fix 2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -82,9 +82,10 @@ class BookDeleteView(generics.DestroyAPIView):
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters import rest_framework
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import filters
 from .models import Book
 from .serializers import BookSerializer
+
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
